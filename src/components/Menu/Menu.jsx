78d146f264453261bf0a95c02ca7e2317af3613d
Alexis Th <thullier.alexis@hotmail.fr>
2025-10-01 14:53:02 +0200

    style(Menu): remake all tailwind to css in mobile first logic

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,42 +1,67 @@
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/img/logo.png";
+import burgerMenu from "../../assets/icons/burgerMenu.svg";
+import userIcon from "../../assets/icons/userIcon.png";
+import { useState } from "react";
 
 export default function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="h-[min-content] p-[1rem] pl-[2rem] pr-[2rem] bg-[#FDF0D5]">
-      <nav className="flex justify-between items-center">
-        <NavLink className="flex gap-[0.5rem] items-center">
-          <img src={logo} className="w-[3rem]" />
-          <span className="font-robotoCond font-[700] text-[1.4rem]">
-            BrickNest
-          </span>
-        </NavLink>
-        <ul className="flex gap-[1.2rem]">
-          <li>
-            <NavLink className="hover:text-[#669BBC]" to="#">
-              Actualités
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="hover:text-[#669BBC]" to="#">
-              Parcourir les sets
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="hover:text-[#669BBC]" to="#">
-              Rechercher un utilisateur
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="text-white bg-[#C1121F] hover:bg-[#780000] p-[0.8rem] rounded-[0.5rem]"
-              to="#"
-            >
-              S'inscrire/Se connecter
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <div className="h-[min-content] p-[1rem] bg-[#FDF0D5] flex justify-between relative">
+      <button
+        className="flex-1 lg:hidden cursor-pointer"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <img src={burgerMenu} className="w-[1.6rem] sm:w-[2rem]" />
+      </button>
+      <NavLink
+        className="flex-1 flex flex-col lg:flex-row lg:gap-[0.6rem] items-center"
+        to="#"
+      >
+        <img className="w-[2rem] sm:w-[2.4rem] lg:w-[3rem]" src={logo} />
+        <span className="font-robotoCond font-[700] text-[0.8rem] sm:text-[1.2rem]">
+          BrickNest
+        </span>
+      </NavLink>
+      <ul
+        className={`absolute lg:static transition-all duration-300 ${
+          isOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 lg:opacity-100 pointer-events-none lg:pointer-events-auto"
+        }  top-[4.8rem] sm:top-[5.2rem] bg-[#FDF0D5] left-0 p-[1rem] flex flex-col lg:flex-row gap-[1rem] transition-all duration-300 text-[0.8rem] sm:text-[1rem] rounded-br-[0.5rem]`}
+      >
+        <li>
+          <NavLink to="#">Actualités</NavLink>
+        </li>
+        <li className="bg-black h-[0.01rem] lg:hidden "></li>
+        <li>
+          <NavLink to="#">Catalogue</NavLink>
+        </li>
+        <li className="bg-black h-[0.01rem] lg:hidden "></li>
+        <li>
+          <NavLink to="#">Chercher un utilisateur</NavLink>
+        </li>
+        <li className="hidden lg:block font-nunito text-white">
+          <NavLink
+            className="bg-[#C1121F] hover:bg-[#780000] p-[0.5rem] rounded-[0.5rem]"
+            to="#"
+          >
+            S'inscrire/Se connecter
+          </NavLink>
+        </li>
+      </ul>
+      <NavLink
+        to="#"
+        className="flex-1 flex justify-end items-center lg:hidden"
+      >
+        <img
+          src={userIcon}
+          className="w-[2rem] h-[2rem] sm:w-[2.4rem] sm:h-[2.4rem]"
+        />
+      </NavLink>
     </div>
   );
 }
