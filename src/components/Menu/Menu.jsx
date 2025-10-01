f56668431f2b97bbc7e21767a1a39f0ab5294c74
Alexis Th <thullier.alexis@hotmail.fr>
2025-10-01 10:28:55 +0200

    feat(Menu): create 3 diffrents menu components files

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,12 @@
+export default function Menu() {
+  return (
+    <div className="h-[min-content] p-[0.5rem] pl-[2rem] pr-[2rem] bg-[#FAF0E2]">
+      <nav>
+        <div className="flex gap-[0.5rem] items-center">
+          <img src="../public/img/logo.png" className="w-[3rem]" />
+          <span className="font-robotoCond">BrickNest</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
