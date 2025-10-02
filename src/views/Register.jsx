a3ed5afa966c5ad701a07abc7cf3206e0312bebf
Alexis Th <thullier.alexis@hotmail.fr>
2025-10-02 15:06:23 +0200

    feat(Register): add yup errors message and automatic enable register button on valid inputs

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -8,13 +8,15 @@ export default function Register() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(registerSchema), mode: "onBlur" });
+    formState: { errors, isValid },
+  } = useForm({ resolver: yupResolver(registerSchema), mode: "onChange" });
 
-  console.log(errors.username);
+  function registerAccount(data) {
+    console.log(data);
+  }
 
   return (
-    <div className="lg:w-[100%] lg:h-[min-content] lg:rounded-[10rem] lg:rounded-[1rem] lg:flex lg:justify-center lg:mt-[10%]">
+    <div className="lg:w-[100%] lg:h-[min-content] lg:rounded-[10rem] lg:rounded-[1rem] lg:flex lg:justify-center lg:mt-[20%] xl:mt-[10%]">
       <div
         className="bg-cover bg-center h-[100vh] lg:h-[100%] relative lg:static lg:rounded-[1rem] lg:w-[40rem]"
         style={{ backgroundImage: `url(${avengersTower})` }}
@@ -24,68 +26,113 @@ export default function Register() {
             <h1 className="font-robotoCond font-[700] text-white text-[2rem] lg:text-[2.8rem]">
               Inscription
             </h1>
-            <form className="flex flex-col items-center text-white mt-[0.5rem] lg:mt-[2rem] w-[50%] md:w-[20rem]">
+            <form
+              onSubmit={handleSubmit(registerAccount)}
+              className="flex flex-col items-center text-white mt-[0.5rem] lg:mt-[2rem] w-[50%] md:w-[20rem]"
+            >
               <div className="lg:flex lg:gap-[1rem] w-[100%]">
                 <div className="flex flex-col items-center lg:items-start">
-                  <label className="text-[1rem]" for="lastname">
+                  <label
+                    className="text-[1rem] md:text-[1.2rem] mt-[1rem]"
+                    for="lastname"
+                  >
                     Nom
                   </label>
                   <input
                     {...register("lastname")}
-                    className="bg-white text-black rounded-[0.5rem] p-[0.2rem] mb-[1rem] w-[100%]"
+                    className="bg-white text-black rounded-[0.5rem] p-[0.2rem] w-[100%]"
                     type="text"
                     name="lastname"
                   />
-                  {errors.lastname && <p>{errors.lastname.message}</p>}
+                  {errors.lastname && (
+                    <p className="text-[0.6rem] md:text-[0.8rem] ">
+                      {errors.lastname.message}
+                    </p>
+                  )}
                 </div>
                 <div className="flex flex-col items-center lg:items-start">
-                  <label className="text-[1rem]" for="firstname">
+                  <label
+                    className="text-[1rem] md:text-[1.2rem] mt-[1rem]"
+                    for="firstname"
+                  >
                     Prénom
                   </label>
                   <input
-                    className="bg-white text-black rounded-[0.5rem] p-[0.2rem] mb-[1rem] w-[100%]"
+                    className="bg-white text-black rounded-[0.5rem] p-[0.2rem]  w-[100%]"
                     type="text"
                     {...register("firstname")}
                     name="firstname"
                   />
-                  {errors.firstname && <p>{errors.firstname.message}</p>}
+                  {errors.firstname && (
+                    <p className="text-[0.6rem] md:text-[0.8rem]">
+                      {errors.firstname.message}
+                    </p>
+                  )}
                 </div>
               </div>
-              <label className="text-[1rem] lg:self-start" for="username">
+              <label
+                className="text-[1rem] md:text-[1.2rem] mt-[1rem] lg:self-start"
+                for="username"
+              >
                 Nom d'utilisateur
               </label>
               <input
-                className="bg-white text-black rounded-[0.5rem] p-[0.2rem] mb-[1rem] w-[100%]"
+                className="bg-white text-black rounded-[0.5rem] p-[0.2rem] w-[100%]"
                 type="text"
                 {...register("username")}
                 name="username"
               />
-              {errors.username && <p>{errors.username.message}</p>}
-              <label className="text-[1rem] lg:self-start" for="mail">
+              {errors.username && (
+                <p className="text-[0.6rem] md:text-[0.8rem] lg:self-start">
+                  {errors.username.message}
+                </p>
+              )}
+              <label
+                className="text-[1rem] md:text-[1.2rem] mt-[1rem] lg:self-start"
+                for="mail"
+              >
                 Mail
               </label>
               <input
-                className="bg-white text-black rounded-[0.5rem] p-[0.2rem] mb-[1rem] w-[100%]"
+                className="bg-white text-black rounded-[0.5rem] p-[0.2rem] w-[100%]"
                 type="mail"
                 {...register("mail")}
                 name="mail"
               />
-              {errors.mail && <p>{errors.mail.message}</p>}
-              <label className="text-[1rem] lg:self-start" for="password">
+              {errors.mail && (
+                <p className="text-[0.6rem] md:text-[0.8rem] lg:self-start">
+                  {errors.mail.message}
+                </p>
+              )}
+              <label
+                className="text-[1rem] md:text-[1.2rem] mt-[1rem] lg:self-start"
+                for="password"
+              >
                 Mot de passe
               </label>
               <input
-                className="bg-white text-black rounded-[0.5rem] p-[0.2rem] mb-[1rem] w-[100%]"
+                className="bg-white text-black rounded-[0.5rem] p-[0.2rem] w-[100%]"
                 type="password"
                 {...register("password")}
                 name="password"
               />
-              {errors.password && <p>{errors.password.message}</p>}
-              <button className="bg-[#003049] mt-[0.5rem] p-[1rem] rounded-[0.5rem] text-[1rem] hover:bg-[#669BBC] font-[700]">
+              {errors.password && (
+                <p className="text-[0.6rem] md:text-[0.8rem] lg:self-start">
+                  {errors.password.message}
+                </p>
+              )}
+              <button
+                disabled={!isValid}
+                className={` bg-[#003049] mt-[1.5rem] p-[1rem] rounded-[0.5rem] text-[1rem] md:text-[1.2rem]  font-[700] ${
+                  !isValid
+                    ? "opacity-50"
+                    : "opacity-100 hover:bg-[#669BBC] cursor-pointer"
+                }`}
+              >
                 S'inscrire
               </button>
             </form>
-            <span className="mt-[0.5rem] text-white">
+            <span className="mt-[0.5rem] md:text-[1.2rem] text-white ">
               Vous avez déjà un compte ?{" "}
               <Link className="underline">Connectez-vous</Link>
             </span>
