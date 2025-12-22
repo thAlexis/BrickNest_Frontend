6d4f2e6ff6d3766924dead5f65832d073d953d0b
Alexis Thullier <thullier.alexis@hotmail.fr>
2025-12-22 15:41:32 +0100

    add log out homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MenuUser from "./components/Menu/MenuUser";
 import AppRoutes from "./router/route";
 import MenuUserBottom from "./components/Menu/MenuUserBottom";
 import { UserContext } from "./contexts/UserContext";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const { userDatas } = useContext(UserContext);
@@ -14,6 +15,7 @@ function App() {
       {userDatas ? <MenuUser /> : <Menu />}
       {userDatas && <MenuUserBottom />}
       <AppRoutes />
+      <Footer />
     </>
   );
 }
